3b5185ef4fc016affb566ef9c7f40de83be122e7
Uifea <131677877+Uifea@users.noreply.github.com>
2023-09-26 10:59:08 +0300

    Add files via upload

--- practic_1/script.js
+++ practic_1/script.js
@@ -3,6 +3,10 @@ let modalWindowBody = document.querySelector(".modal__body");
 let buttonOpenModal = document.querySelector(".main__btn");
 let buttonCloseModal = document.querySelector(".modal__close");
 
+let image = document.getElementById("logo-image");
+let file = document.getElementById("logo-input");
+let cancelUpload = document.querySelector(".modal__logo-close");
+
 
 //Функция по закрыванию и открыванию попапа
 let toggleModal = function () {
@@ -27,3 +31,12 @@ buttonOpenModal.addEventListener('click', () => {
     toggleModal();
 })
 
+
+file.addEventListener('change', function(){
+    image.src = URL.createObjectURL(file.files[0]);
+    image.style.display = "block";
+});
+
+cancelUpload.addEventListener('click', function() {
+    image.src = "img/Ellipse 44.png";
+})
